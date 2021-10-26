3555c9b0dcb4e4412fa5cc9a72fa740455c6e5bd
MaxShmurov <ma1997ks@gmail.com>
2021-10-26 20:14:39 +0300

    Camera follow and autorun

--- Misery 2/Assets/Scripts/PlayerMovement.cs
+++ Misery 2/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,14 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5;
     public Rigidbody rb;
 
+    float horizontalInput;
+    public float horizontalMultiplier = 2;
+
     private void FixedUpdate()
     {
         Vector3 forwardMove = transform.forward * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + forwardMove);
+        Vector3 horizontalMove = transform.right * horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier;
+        rb.MovePosition(rb.position + forwardMove + horizontalMove);
     }
 
 
@@ -17,6 +21,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 }
