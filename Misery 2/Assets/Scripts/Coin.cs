ea06412de87258655337bba7b0d5c2b486b04f57
MaxShmurov <ma1997ks@gmail.com>
2021-10-29 19:52:31 +0300

    with coins, score, restart, pt4 completed

--- Misery 2/Assets/Scripts/Coin.cs
+++ Misery 2/Assets/Scripts/Coin.cs
@@ -7,12 +7,18 @@ public class Coin : MonoBehaviour
     public float turnSpeed = 90f;
     private void OnTriggerEnter(Collider other)
     {
+        if(other.gameObject.GetComponent<Obstacle>() != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         if (other.gameObject.name != "Player")
         {
             return;
-
-
         }
+        GameManager.inst.IncrementScore();
+
         Destroy(gameObject);
 
     }
